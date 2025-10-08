6879289752ec37425ff5b8bd7bd062811cef3bf6
Bryan Powell <powell.bryan@outlook.com>
2025-10-08 16:48:11 -0400

    feat: vite and tailwind

---
+++ apps/dashboard/src/app/app.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button className="bg-blue-500 text-white p-2 rounded-md">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
