0f4b2efab213dbee9855341d9693eda7f8448d11
Bryan Powell <powell.bryan@outlook.com>
2025-10-08 19:01:59 -0400

    feat: more tweaks and testing shad

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,5 +1,11 @@
 import { Button } from '@longpoint/web-ui/components/button';
 import { Badge } from '@longpoint/web-ui/components/badge';
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@longpoint/web-ui/components/card';
 
 export function App() {
   return (
@@ -7,6 +13,12 @@ export function App() {
       <h1>Hello World</h1>
       <Button>Click Me</Button>
       <Badge>Dis a badge</Badge>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
